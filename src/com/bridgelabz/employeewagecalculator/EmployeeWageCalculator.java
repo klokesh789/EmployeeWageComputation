9aa5eb7ad239e48a59962eb0f22cc32b348504c4
lokesh <kottelokesh789@gmail.com>
2021-12-18 17:30:36 +0530

    ADDED: SolvedBySwitchStatement

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
@@ -1,22 +1,24 @@
 package com.bridgelabz.employeewagecalculator;
 
+
 public class EmployeeWageCalculator {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
 	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if ( empCheck == IS_FULL_TIME ) {			
+		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+		switch( empCheck ) {
+		case 2: 			
 			System.out.println("Employee is present ");
 			empHrs = 8;
-		}
-		else if( empCheck == IS_PART_TIME ) {
+			break;
+		case 1 :
 			System.out.println("Employee doing part-time ");
 			empHrs = 8;
-		}
-		else { 
+			break;
+		default:
 			System.out.println("Employee is Absent ");
 			empHrs = 0;
 		}
