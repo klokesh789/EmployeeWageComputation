a9a480dda7bc84165409ee7afe102756ad76f803
lokesh <kottelokesh789@gmail.com>
2021-12-18 17:14:47 +0530

    ADDED: Part-TimeWage

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
@@ -3,12 +3,17 @@ package com.bridgelabz.employeewagecalculator;
 public class EmployeeWageCalculator {
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if ( empCheck == IS_FULL_TIME ) {			
 			System.out.println("Employee is present ");
+			empHrs = 8;
+		}
+		else if( empCheck == IS_PART_TIME ) {
+			System.out.println("Employee doing part-time ");
 			empHrs = 8;
 		}
 		else { 
