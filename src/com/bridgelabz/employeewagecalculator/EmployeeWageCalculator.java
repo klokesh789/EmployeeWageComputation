e899589b542c642050d80b9123b31e7174be63cd
lokesh <kottelokesh789@gmail.com>
2021-12-18 17:06:53 +0530

    ADDED: EmployeeDailyWage

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
@@ -3,11 +3,21 @@ package com.bridgelabz.employeewagecalculator;
 public class EmployeeWageCalculator {
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if ( empCheck == IS_FULL_TIME )
-		System.out.println("Employee is present ");
-		else 
-		System.out.println("Employee is Absent ");
+		if ( empCheck == IS_FULL_TIME ) {			
+			System.out.println("Employee is present ");
+			empHrs = 8;
 		}
+		else { 
+			System.out.println("Employee is Absent ");
+			empHrs = 0;
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
+
+	}
 
 }
